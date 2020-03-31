800196ddc4706009ff482658e93a6c55e10b88ca
JGGuerrero <jgguerrero@mail.fresnostate.edu>
2020-03-30 18:42:31 -0700

    finish practice

---
+++ contact.ts
@@ -0,0 +1,16 @@
+
+class Contact {
+
+    constructor() {
+
+    }
+
+
+    goToHome() {
+        window.location.href = 'index.html';
+    }
+
+}
+
+
+const contact = new Contact();
---
+++ login.ts
@@ -0,0 +1,50 @@
+class Login {
+
+
+    constructor() {
+
+    }
+
+    login() {
+        const usernameInput = document.getElementById('username');
+        // console.log('from login() usernameInput', usernameInput);
+
+        const passwordInput = document.getElementById('password');
+
+        let username = usernameInput.value;
+        let password = passwordInput.value;
+
+
+        // console.log('username', username)
+        username = usernameInput.value
+        // console.log('password', password)
+
+        if(username === 'user' && password === 'abc123') {
+            this.goToHome();
+        }else {
+            this.displayError();
+        }
+    }
+
+    displayError() {
+        const errorBox = document.getElementById('error-box');
+        errorBox.innerHTML = 'User not found!';
+        // console.log('errorBox.innerHTML', errorBox.innerHTML);
+
+        errorBox.style = "display: block;";
+        const interval = setInterval(() => {
+            errorBox.style = "display: none;";
+            clearInterval(interval);
+        }, 5000);
+
+        
+    }
+
+    goToHome () {
+        window.location.href = 'index.html'
+    }
+
+
+}
+
+const login = new Login();
